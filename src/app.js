7d293214faa256e96dc838575a22f80730efc88b
chinmay keni <137027001+chinmaykeni9422@users.noreply.github.com>
2023-12-22 14:41:03 +0530

    app server file

---
+++ src/app.js
@@ -0,0 +1,32 @@
+import  Express  from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+
+const app = Express();
+
+
+
+
+// cors configuration
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}));
+
+//telling server that you can accept json format data and setting its limit
+app.use(Express.json({limit: "20kb"}));
+
+// encoding url and setting limit
+app.use(Express.urlencoded({limit: "20kb"}));
+
+//---
+app.use(Express.static("public")) ;
+
+// cookies configuration
+app.use(cookieParser()) ;
+
+
+
+
+export default app ;
