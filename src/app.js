a1f170de694171fb0a11848c8ad8673dc573055a
chinmay keni <137027001+chinmaykeni9422@users.noreply.github.com>
2024-01-05 10:24:20 +0530

    app server file

--- src/app.js
+++ src/app.js
@@ -28,5 +28,13 @@ app.use(cookieParser()) ;
 
 
 
+//Routes import
+import userRouter from './routes/user.routes.js'
+ 
+
+//routes declaration
+app.use("/api/v1/users", userRouter)
+
+
 
 export default app ;
