488ba654b7b4420dedfbbf57e0a08ae4bf0ce518
chinmay keni <137027001+chinmaykeni9422@users.noreply.github.com>
2024-02-24 10:19:48 +0530

    working on mail system

--- src/controllers/Arduino.controller.js
+++ src/controllers/Arduino.controller.js
@@ -1,4 +1,5 @@
 import User from "../models/user.model.js";
+import nodemailer from "nodemailer"
 
 const handleData =  async (data, Id) => {
 
@@ -13,6 +14,17 @@ const handleData =  async (data, Id) => {
             existingData.moisture = moistureData.moisture;
             const updateData = await existingData.save();
             // console.log("Moisture data updated for user:", updateData.moisture);
+
+
+            // Call the sendEmail function with the userEmail
+            if (updateData.moisture < 900) {
+                const subject = 'Moisture Alert';
+                const text = `Moisture level is low! Please take action.`;
+            
+                // Send an email to the logged-in user
+                sendEmail(process.env.TOEMAIL, subject, text);
+              }
+
         } else {
             console.log("no existing user found")
         }
@@ -28,5 +40,31 @@ const handleData =  async (data, Id) => {
     }
 
 }
+
+// Function to send email
+const sendEmail = (to, subject, text) => {
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: process.env.USEREMAIL,
+        pass: process.env.USEREMAILPASSWORD
+      }
+    });
+  
+    const mailOptions = {
+      from: process.env.USEREMAIL,
+      to: to,
+      subject: subject,
+      text: text,
+    };
+  
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('Email sent: ' + info.response);
+      }
+    });
+  }
 
 export {handleData} ;
